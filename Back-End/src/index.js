5fee7cc71d37156c894142a4e2770b8c8e2da39c
Gustavo Leoteau <Gustavoleoteau3@gmail.com>
2023-01-25 15:02:01 -0500

    change: rutas movidas a otro archivos

--- Back-End/src/index.js
+++ Back-End/src/index.js
@@ -1,50 +1,10 @@
 const express = require('express')
 const app = express()
+const ruta = require('../Routers/routers')
 
-app.use(express.json())
+app.use(ruta)
 
-var Notas = [];
 
-app.get('/',(req,res) => {
-    res.send('<h1>Funcionando</h1>')
-})
-
-app.get('/notas',(req,res) => {
-    res.json(Notas)
-})
-app.get('/notas/nota/:id',(req,res)=>{
-    const id = Number(req.params.id)
-    const Nota = Notas.find((Notas)=>{
-    return (Notas.id === id)})
-    res.json(Nota)
-})
-
-app.post('/notas/nota',(req,res)=>{
-    const a = req.body
-    var idMax
-    if(Notas.length === 0)
-    {
-        idMax = 0;
-    }
-    else{
-        const id = Notas.map(Note => Note.id)
-        idMax = Math.max(...id)
-    }
-    
-    const newNota = {
-        id:  idMax + 1,
-        content: a.title,
-        descript: a.desc,
-    }
-    res.json(newNota)
-    Notas = [...Notas,newNota]
-})
-
-app.delete('/notas/nota/:id',(req,res) => {
-    const id = Number(req.params.id)
-    Notas = Notas.filter(Notas => Notas.id !== id)
-    res.json(Notas)
-})
 const port = 3001
 app.listen(port,() =>{
     console.log(`La pagina web se esta generando en el port ${port}`)
