540064a308b478f71152f0fc2eac05bfd975188b
Gustavo Leoteau <Gustavoleoteau3@gmail.com>
2023-02-02 17:36:07 -0500

    change: modificacion de rutas

--- Back-End/Routers/routers.js
+++ Back-End/Routers/routers.js
@@ -1,53 +1,50 @@
 const express = require('express')
-const Router = express.Router()
-const MD = express()
-MD.use(express.json())
+const app = express()
+
 var Notas = [];
 
 
-Router.get('/',(req,res) => {
+app.get('/',(req,res) => {
         res.send('<h1>Funcionando</h1>')
     })
 
-Router.get('/notas',(req,res) => {
+app.get('/notas',(req,res) => {
         res.json(Notas)
     })
 
-Router.get('/notas/nota/:id',(req,res)=>{
+app.get('/notas/nota/:id',(req,res)=>{
         const id = Number(req.params.id)
         const Nota = Notas.find((Notas)=>{
         return (Notas.id === id)})
         res.json(Nota)
     })
-const Notapostear = (params) =>{
-    Router.post('/notas/nota',(req,res)=>{
-        const a = req.body
-        var idMax
-        if(Notas.length === 0)
-        {
-            idMax = 0;
-        }
-        else{
-            const id = Notas.map(Note => Note.id)
-            idMax = Math.max(...id)
-        }
-        
-        const newNota = {
-            id:  idMax + 1,
-            content: params.titulo,
-            descript: params.descrip,
-        }
-        res.json(newNota)
-        Notas = [...Notas,newNota]
-    })
-}
 
+app.post('/notas/nota',express.urlencoded({extended:false}),(req,res)=>{
+    const titulo = req.body.titulo
+    const descrip = req.body.descrip
+    var idMax
+    if(Notas.length === 0)
+    {
+        idMax = 0;
+    }
+    else{
+        const id = Notas.map(Note => Note.id)
+        idMax = Math.max(...id)
+    }
+    
+    const newNota = {
+        id:  idMax + 1,
+        content: titulo,
+        descript: descrip,
+    }
+    Notas = [...Notas,newNota]
+    res.status(201)
+})
 
-Router.delete('/notas/nota/:id',(req,res) => {
+app.delete('/notas/nota/:id',(req,res) => {
         const id = Number(req.params.id)
         Notas = Notas.filter(Notas => Notas.id !== id)
         res.json(Notas)
     })
 
-module.exports = Router
-module.exports = Notapostear
+module.exports = app
--- Back-End/src/index.js
+++ Back-End/src/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const ruta = require('../Routers/routers')
 
+app.use(cors())
+app.use(express.json())
 app.use(ruta)
 
 
